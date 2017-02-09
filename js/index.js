8c735a9f4e1b437b87e45c77ee2b686896ad2ff7
Alexander Khotemskoy <Xotabu4@gmail.com>
2017-02-09 18:34:47 +0200

    Added tests, removed extending of arrayelementfinder since it will be moved to 2.0.0
    Added tests, removed extending of arrayelementfinder since it will be
    moved to 2.0.0

--- js/index.js
+++ js/index.js
@@ -1,38 +1,14 @@
 "use strict";
-const protractor_1 = require('protractor');
-class BaseElement extends protractor_1.ElementFinder {
+const protractor_1 = require("protractor");
+/**
+ * This class describe basic fragment that you can use as basic object to inherit
+ * Basically this uses provided element browser_ and elementArrayFinder_
+ * and use it in ElementFinder constructor.
+ */
+class BaseFragment extends protractor_1.ElementFinder {
     constructor(extendable) {
         super(extendable.browser_, extendable.elementArrayFinder_);
     }
 }
-exports.BaseElement = BaseElement;
-class BaseElementArray extends protractor_1.ElementArrayFinder {
-    constructor(extendable, class_) {
-        super(extendable.browser_, extendable.getWebElements, extendable.locator_, extendable.actionResults_);
-        this.class_ = class_;
-    }
-    //TODO: Still want to add typings here. Need to continue experiments with generics.
-    get(indx) {
-        return new this.class_(super.get(indx));
-    }
-    first() {
-        return new this.class_(super.first());
-    }
-    last() {
-        return new this.class_(super.last());
-    }
-    map(func) {
-        //TODO: return result
-        return this.map(function (elFinder, index) {
-            return func(new this.class_, index);
-        });
-    }
-    filter(func) {
-        //TODO: return result
-        return this.filter(function (elFinder, index) {
-            return func(new this.class_, index);
-        });
-    }
-}
-exports.BaseElementArray = BaseElementArray;
+exports.BaseFragment = BaseFragment;
 //# sourceMappingURL=index.js.map
